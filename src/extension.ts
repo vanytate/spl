d7761027d679054d5f1935cc92a1743ea06994bf
George Cherchel <george.cherchel@inventorsoft.co>
2017-05-07 20:59:46 +0300

    Add snippets.

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,26 @@
 'use strict';
+
 import * as vscode from 'vscode'
+import { languages, TextDocument, Position, CompletionItem, TextEditor } from 'vscode'
 import  { MachineController } from './MachineController'
 import keywords from './keywords'
-
 
 export function activate(context: vscode.ExtensionContext) {
 
     new MachineController(keywords)
 
+    // context.subscriptions.push(vscode.languages.registerCompletionItemProvider('spl', {
+    //     provideCompletionItems(document: TextDocument, position: Position) {
+    //         console.log(document.getWordRangeAtPosition(position))
+    //         return completions;
+    //     }
+    // }));
+
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
 
-        vscode.window.showInformationMessage('Hello World!');
+        let k = new vscode.CompletionItem("asdfasd", vscode.CompletionItemKind.Keyword)
+
+        
     });
 
     context.subscriptions.push(disposable);
@@ -19,3 +29,5 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
+
+// vscode.window.showInformationMessage('Hello World!');
